37b68cf30379897d5efdeb34a9d52a07e0c4c728
Fabian Waschkowski <fwaschkowski@student.unimelb.edu.au>
2020-04-06 10:34:02 +1000

    Change homemadeNN to class

--- homemadeNN.py
+++ homemadeNN.py
@@ -1,291 +1,301 @@
-"""Homemade code to train and test 
+"""Homemade artificial neural network.
 
-Todo:
-- speed up with matrix multiplication
-- ensure proper matrix multiplication everywhere
-- clean up
+To do:
+- research gradient descent improvements
+- generalize cost function
+- generalize activation function
+- change w_mats to self.w_mats in all functions
+- adapted learning rate
+- relaxation of weights
 """
-
 import numpy as np
-import pandas as pd
-import os
 import time
 
-np.random.seed(0)
+class HomemadeNN(object):
+    """Homemade artificial neural network."""
 
-def act_fun(z_mat):
-    return (1 / (1 + np.exp(-1 * np.array(z_mat))))
+    def __init__(self, size_list, seed=-1):
+        """Initialize neural network and random weights.
 
-def forward_prop(x_mat, w_mats, layer=-1):
-        
-    if layer == -1:
-        layer = len(w_mats)
+        Args:
+            size_list: Number of neurons per layer.
+            seed: Seed for numpy.random generator.
+        """
 
-    if len(x_mat.shape) == 1:
-        x_mat = x_mat[np.newaxis]
+        # define NN size
+        self.s = size_list
+        self.K = size_list[-1]
+        self.L = len(self.s)
 
-    a_list = []
-    a = np.insert(x_mat, 0, 1, axis=1)
-    a_list.append(a)
-
-    for i in range(layer):
-
-        # select weight matrix
-        w_mat = w_mats[i]
-
-        # 2D dot operation
-        z = a.dot(w_mat.T)
-
-        # multiply activation function
-        a = act_fun(z)
+        # set random seed
+        self.seed = seed
+        if self.seed >= 0:
+            np.random.seed(self.seed)
     
-        # add bias expect for output layer
-        if i < len(w_mats)-1:
-            a = np.insert(a, 0, 1, axis=1)
+        # initialize random weights in range of -eps to eps    
+        self.w_mats = []
+        for i in range(self.L-1):
+        
+            random_mat = np.random.rand(self.s[i+1],self.s[i]+1)
+            eps = np.sqrt(6) / np.sqrt(self.s[i] + self.s[i+1])
 
-        # add to list
+            weight_mat = random_mat * 2 * eps - eps
+            
+            self.w_mats.append(weight_mat)
+
+    def act_fun(self, z_mat):
+        """Calculate element-wise sigmoid activation function."""
+        
+        return (1 / (1 + np.exp(-1 * np.array(z_mat))))
+
+
+    def forward_prop(self, x_mat, w_mats, layer=-1):
+        """ Do forward propagation.
+
+        Args:
+            x_mat: Input data of shape (data points)x(input layer).
+            w_mats: Neural networks weights for each layer.
+            layer: Do forward prop only up to indicated layer index.
+
+        Returns:
+            a_list: Activation values for each layer including bias.
+        """
+        
+        # do full propagation if no layer index indicated
+        if layer == -1:
+            layer = len(w_mats)
+
+        # ensure two-dim. matrix shape
+        if len(x_mat.shape) == 1:
+            x_mat = x_mat[np.newaxis]
+
+        # input layer: no activation
+        a_list = []
+        a = np.insert(x_mat, 0, 1, axis=1)
         a_list.append(a)
 
-    return a_list
+        # loop over remaining layers
+        for i in range(layer):
 
-def backward_prop(x_data, y_data, w_mats, lam):
-    
-    delta_mats = []
-    d_zero = []
-    for i in range(len(w_mats)):
-        delta_mats.append(np.zeros(w_mats[i].shape))
-        d_zero.append(np.zeros(w_mats[i].shape[1]))
-    D_mats = delta_mats[:]
+            # select weight matrix
+            w_mat = w_mats[i]
 
-    for t in range(x_data.shape[0]):
+            # matrix multiplication
+            z = a.dot(w_mat.T)
+
+            # activation function
+            a = self.act_fun(z)
         
-        d = d_zero[:]
+            # add bias except for output layer
+            if i < len(w_mats)-1:
+                a = np.insert(a, 0, 1, axis=1)
 
-        a = forward_prop(x_data[t,:], w_mats)
-        a = a[-1]
-        d.append((a - y_data[t]).T)
+            # add to list
+            a_list.append(a)
+
+        return a_list
+
+
+    def backward_prop(self, x_data, y_data, w_mats, lam):
+        """ Do backward propagation.
+
+        Args:
+            x_data: Input data of shape (data points)x(input layer).
+            y_data: Label data of shape (data points)x(output layer).
+            w_mats: Neural networks weights for each layer.
+            lam: Regularization parameter.
+
+        Returns:
+            D_mats: Derivative of cost w.r.t. weights for each layer.
+        """
+    
+        # initialize matrices
+        delta_mats = []
+        d = []
+        for i in range(len(w_mats)):
+            delta_mats.append(np.zeros(w_mats[i].shape))
+            d.append(np.zeros((x_data.shape[0],w_mats[i].shape[1])))
+        D_mats = delta_mats[:]
+
+        # do forward propagation to get all activations
+        a = self.forward_prop(x_data, w_mats)
+
+        # calculate last d (simple because derivative of cost and activation function match)
+        d.append(a[-1] - y_data) #TODO: change here to generalize for different cost and activation functions
 
         for l in reversed(range(len(w_mats))):
 
+            # select weight matrix
             w_mat = w_mats[l]
-            wd = w_mat.T.dot(d[l+1])
-            
-            a = forward_prop(x_data[t], w_mats, l)
-            a = a[-1].T
 
-            d[l] = np.array([wd[k] * a[k] * (1-a[k]) for k in range(len(a))])
-            d[l] = d[l][1:]
+            # calculate d vector of layer
+            wd = d[l+1].dot(w_mat)
+            d[l] = wd * a[l] * (1 - a[l]) #TODO: change here to generalize for different cost and activation functions
 
-            delta_mats[l] += d[l+1].dot(a.T)
+            # lose d values for bias nodes
+            d[l] = d[l][:,1:]
 
-    # add regularization
-    for l in range(len(w_mats)):
-        w_mat = w_mats[l]
-        reg_w_mat = np.insert(w_mat[:,1:], 0, 0, axis=1)
-        
-        D_mats[l] = 1 / x_data.shape[0] * (delta_mats[l] + lam * reg_w_mat)
+            # calculate delta matrix
+            delta_mats[l] = d[l+1].T.dot(a[l])
 
-    return D_mats
+            # add regularization
+            reg_w_mat = np.insert(w_mat[:,1:], 0, 0, axis=1)
+            D_mats[l] = 1 / x_data.shape[0] * (delta_mats[l] + lam * reg_w_mat)
 
-def backward_prop_fast(x_data, y_data, w_mats, lam):
+        return D_mats
+
+
+    def cost_fun(self, x_data, y_data, w_mats, lam):
+        """ Calculate logarithmic cost function.
+
+        Args:
+            x_data: Input data of shape (data points)x(input layer).
+            y_data: Label data of shape (data points)x(output layer).
+            w_mats: Neural networks weights for each layer.
+            lam: Regularization parameter.
+
+        Returns:
+            cost: Logarithmic cost function value including regularization.
+        """
     
-    delta_mats = []
-    d = []
-    for i in range(len(w_mats)):
-        delta_mats.append(np.zeros(w_mats[i].shape))
-        d.append(np.zeros((x_data.shape[0],w_mats[i].shape[1])))
-    D_mats = delta_mats[:]
-
-    a = forward_prop(x_data, w_mats)
-    d.append(a[-1] - y_data)
-
-    for l in reversed(range(len(w_mats))):
-
-        w_mat = w_mats[l]
-
-        wd = d[l+1].dot(w_mat)
-        d[l] = wd * a[l] * (1 - a[l])
-
-        d[l] = d[l][:,1:]
-
-        delta_mats[l] = d[l+1].T.dot(a[l])
+        # calculate cost
+        h = self.forward_prop(x_data, w_mats)[-1]
+        cost = -1 / x_data.shape[0] * (np.trace(y_data.dot(np.log(h).T)) + np.trace((1 - y_data).dot(np.log(1 - h).T)))
 
         # add regularization
-        reg_w_mat = np.insert(w_mat[:,1:], 0, 0, axis=1)
-        D_mats[l] = 1 / x_data.shape[0] * (delta_mats[l] + lam * reg_w_mat)
-
-    return D_mats
-
-
-def cost_fun(x_data, y_data, w_mats, lam):
-    
-    m = x_data.shape[0]
-    K = w_mats[-1].shape[0]
-    
-    cost = 0
-    for t in range(m):
-        for k in range(K):
-            cost += y_data[t, k] * np.log(forward_prop(x_data[t], w_mats)[-1][:,k]) + (1 - y_data[t, k]) * np.log(1 - forward_prop(x_data[t], w_mats)[-1][:,k])
-
-    cost *= -1 / m
-
-    # add regularization
-    reg_cost = 0
-
-    for l in range(len(w_mats)):
-        w_mat = w_mats[l]
-
-        for i in range(1, w_mat.shape[1]):
-            for j in range(w_mat.shape[0]):
-                reg_cost += w_mat[j,i]**2
-
-    reg_cost *= lam / 2 / m
-
-    return cost + reg_cost
-
-def cost_fun_fast(x_data, y_data, w_mats, lam):
-    
-    # calculate cost
-    h = forward_prop(x_data, w_mats)[-1]
-    cost = -1 / x_data.shape[0] * (np.trace(y_data.dot(np.log(h).T)) + np.trace((1 - y_data).dot(np.log(1 - h).T)))
-
-    # add regularization
-    reg_cost = 0
-    
-    for l in range(len(w_mats)):
-        w_mat = w_mats[l]
-
-        reg_w_mat = w_mat[:,1:]
+        reg_cost = 0
         
-        reg_cost += np.trace(reg_w_mat.dot(reg_w_mat.T))
+        for l in range(len(w_mats)):
+            w_mat = w_mats[l]
 
-    reg_cost *= lam / 2 / x_data.shape[0]
+            reg_w_mat = w_mat[:,1:]
+            
+            reg_cost += np.trace(reg_w_mat.dot(reg_w_mat.T))
 
-    return cost + reg_cost
-
-
-def grad_descent(w_mats, D_mats, alpha):
-
-    for l in range(len(w_mats)):
-        w_mats[l] = w_mats[l] - alpha * D_mats[l]
-
-    return w_mats
+        reg_cost *= lam / 2 / x_data.shape[0]
         
-
-if __name__ == "__main__":
-    
-    ### Neural network parameters
-
-    # # Number of layers including input and output layer
-    # L = 3
-
-    # # Number of units in each layer without bias unit
-    # s = [4, 4, 1]
-
-    # # Number of units in output layer
-    # K = 1
-
-    # What do we need:
-    # data, split up in training and testing data, initial weights, activation function
-
-    # What do we want to do?
-    # Let's say we have only training example: x with size of (s[0]+1)x1 and y of size 1x1
-    # forward propagation: h = a_1_3 = act()
-
-    print(os.getcwd())
-
-    # data: image size 28x28 - training set size 60k
-    # data = pd.read_csv('learning-machine-learning/data/mnist/mnist_train.csv')
-
-    # x_data = data.iloc[:,1:]
-    # y_data = data.iloc[:,0]
-
-    # data: image size 20x20 - training set size 5k
-    x_data = pd.read_csv('learning-machine-learning/data/mnist/mnist_x_data.csv', header=None)
-    y_data = pd.read_csv('learning-machine-learning/data/mnist/mnist_y_data.csv', header=None)
-
-    # x_data to numpy array - why loading via pandas at all?
-    x_data = np.array(x_data)
-    y_data = np.array(y_data)
-
-    # mix up data
-    data = np.concatenate((y_data, x_data), axis=1)
-    np.random.shuffle(data)
-    x_data = data[:,1:]
-    y_data = data[:,0]
-
-    y_org = y_data[:]
-
-    # y_data to vec form - do as matrix operation?
-    y_vecs = np.zeros((len(y_data), int(max(y_data)-min(y_data))+1))
-
-    for i in range(y_vecs.shape[0]):
-        y_val = y_data[i]
-        if y_val == 10:
-            y_val = 0
-        y_vecs[i, int(y_val)] = 1
-    
-    y_data = y_vecs
-
-    # neural network parameters
-    s_hidden = [25]
-    K = 10
-    lam = 10
-    alpha = 0.5
-
-    alpha_start = 1.0
-    alpha_end = 0.001
-
-    s = [x_data.shape[1]] + s_hidden + [K]
-    L = len(s)
-    
-    # initialize random weights in range of -eps to eps    
-    weight_mats = []
-    for i in range(L-1):
+        # total cost
+        cost += reg_cost
         
-        random_mat = np.random.rand(s[i+1],s[i]+1)
-        eps = np.sqrt(6) / np.sqrt(s[i] + s[i+1])
+        return cost
 
-        weight_mat = random_mat * 2 * eps - eps
+
+    def grad_descent(self, w_mats, D_mats, alpha):
+        """Perform gradient descent.
+
+        Args:
+            w_mats: Neural networks weights for each layer.
+            D_mats: Derivative of cost w.r.t. weights for each layer.
+            alpha: Learning rate.
+
+        Returns:
+            w_mats: Updated NN weights for each layer.
+        """
+
+        for l in range(len(w_mats)):
+            w_mats[l] = w_mats[l] - alpha * D_mats[l]
+
+        return w_mats
+
+
+    def shuffle_data(self, x_data, y_data):
+        """Shuffle order of data points."""
+
+        # get number of output layer nodes
+        s_L = y_data.shape[1]
+
+        # merge data
+        data = np.concatenate((y_data, x_data), axis=1)
         
-        weight_mats.append(weight_mat)
+        # shuffle data
+        np.random.shuffle(data)
+        
+        # split data
+        x_data = data[:,s_L:]
+        y_data = data[:,:s_L]
 
-    # train NN
-    i = 1
-    cost = cost_fun_fast(x_data, y_data, weight_mats, lam)
+        return (x_data, y_data)
 
-    max_iter = 500
-    mini_batch = 5000
 
-    for i in range(max_iter):
+    def test(self, x_data, y_data):
+        """Test neural network performance."""
 
-        start_time = time.time()    
+        return np.sum(np.argmax(self.forward_prop(x_data, self.w_mats)[-1], axis=1) == np.argmax(y_data, axis=1))/y_data.shape[0]
 
-        num_batches = x_data.shape[0] // mini_batch
-        if x_data.shape[0] % mini_batch:
+
+    def predict(self, x_data):
+        """Predict output based on NN weights."""
+
+        return np.argmax(self.forward_prop(x_data, self.w_mats)[-1], axis=1)
+
+    
+    def train(self, x_data, y_data, alpha, lam, max_iter, eps=1e-6, batch_size=0, shuffle_opt=0, quiet=0):
+        """Train neural network.
+
+        Args:
+            x_data: Input data of shape (data points)x(input layer).
+            y_data: Label data of shape (data points)x(output layer).
+            alpha: Learning rate.
+            lam: Regularization parameter.
+            max_iter: Maximum number of iterations.
+            eps: Rel. diff. in cost between to iteration to stop training.
+            batch_size: Number of data points to be used at once for training.
+            shuffle_opt: Shuffle data points in each iteration.
+            quiet: Do not print anything.
+        """
+
+        # calculate number of batches
+        if batch_size == 0:
+            batch_size = x_data.shape[0]
+
+        num_batches = x_data.shape[0] // batch_size
+        if x_data.shape[0] % batch_size:
             num_batches += 1
 
-        alpha = alpha_start + i * (alpha_end-alpha_start) / max_iter
+        # unnecessary
+        x_train = x_data[:]
+        y_train = y_data[:]
+
+        # monitor cost function convergence
+        self.conv_data = [self.cost_fun(x_data, y_data, self.w_mats, lam)]
+
+        for i in range(max_iter):
+
+            if not quiet:
+                start_time = time.time()    
+
+            # shuffle data
+            if shuffle_opt:
+                x_train, y_train = self.shuffle_data(x_data, y_data)
+        
+            # loop over number of batches
+            for b in range(num_batches):
+
+                # get batch data
+                x_batch = x_train[(b*batch_size):((b+1)*batch_size),:]
+                y_batch = y_train[(b*batch_size):((b+1)*batch_size),:]
+
+                # do back propagation
+                D_mats = self.backward_prop(x_batch, y_batch, self.w_mats, lam)
+
+                # do gradient descent
+                self.w_mats = self.grad_descent(self.w_mats, D_mats, alpha)
+
+            # check convergence
+            self.conv_data.append(self.cost_fun(x_data, y_data, self.w_mats, lam))
+            if abs((self.conv_data[-2]-self.conv_data[-1])/self.conv_data[-2]) < eps:
+                break
+
+            # print information
+            if not quiet:            
+                stop_time = time.time()
+                print("Iteration: %d - Cost: %f - Time: %f - Alpha: %f" % (i, self.conv_data[-1], stop_time - start_time, alpha))
+            
+                if (i % 10) == 0:
+                    print("Accuracy: %.2f %%" % (self.test(x_data, y_data)*100))
+
+        if not quiet:
+            print('The End.')
+            
+
     
-        for b in range(num_batches):
-
-            x_batch = x_data[(b*mini_batch):((b+1)*mini_batch),:]
-            y_batch = y_data[(b*mini_batch):((b+1)*mini_batch),:]
-
-            D_mats = backward_prop_fast(x_batch, y_batch, weight_mats, lam)
-            weight_mats = grad_descent(weight_mats, D_mats, alpha)
-        
-        stop_time = time.time()
-        cost = cost_fun_fast(x_data, y_data, weight_mats, lam)
-        print("Iteration: %d - Cost: %f - Time: %f - Alpha: %f" % (i, cost, stop_time - start_time, alpha))
-        
-        if (i % 10) == 0:
-            res = np.sum(np.argmax(forward_prop(x_data, weight_mats)[-1], axis=1) == y_org)
-            print("Accuracy: %.2f %%" % (res/len(y_org)*100))
-
-    # test NN
-    res = np.sum(np.argmax(forward_prop(x_data, weight_mats)[-1], axis=1) == y_org)
-    print("Accuracy: %.2f %%" % (res/len(y_org)*100))
-
-     
-
