de8e4bbcacb3fe8b5bb0a3aa33af08993801edb2
Fabian Waschkowski <fwaschkowski@student.unimelb.edu.au>
2020-04-10 18:02:25 +1000

    Use weights as attribute instead of function argument

--- homemadeNN.py
+++ homemadeNN.py
@@ -49,12 +49,11 @@ class HomemadeNN(object):
         return (1 / (1 + np.exp(-1 * np.array(z_mat))))
 
 
-    def forward_prop(self, x_mat, w_mats, layer=-1):
+    def forward_prop(self, x_mat, layer=-1):
         """ Do forward propagation.
 
         Args:
             x_mat: Input data of shape (data points)x(input layer).
-            w_mats: Neural networks weights for each layer.
             layer: Do forward prop only up to indicated layer index.
 
         Returns:
@@ -63,7 +62,7 @@ class HomemadeNN(object):
         
         # do full propagation if no layer index indicated
         if layer == -1:
-            layer = len(w_mats)
+            layer = len(self.w_mats)
 
         # ensure two-dim. matrix shape
         if len(x_mat.shape) == 1:
@@ -78,7 +77,7 @@ class HomemadeNN(object):
         for i in range(layer):
 
             # select weight matrix
-            w_mat = w_mats[i]
+            w_mat = self.w_mats[i]
 
             # matrix multiplication
             z = a.dot(w_mat.T)
@@ -87,7 +86,7 @@ class HomemadeNN(object):
             a = self.act_fun(z)
         
             # add bias except for output layer
-            if i < len(w_mats)-1:
+            if i < len(self.w_mats)-1:
                 a = np.insert(a, 0, 1, axis=1)
 
             # add to list
@@ -96,13 +95,12 @@ class HomemadeNN(object):
         return a_list
 
 
-    def backward_prop(self, x_data, y_data, w_mats, lam):
+    def backward_prop(self, x_data, y_data, lam):
         """ Do backward propagation.
 
         Args:
             x_data: Input data of shape (data points)x(input layer).
             y_data: Label data of shape (data points)x(output layer).
-            w_mats: Neural networks weights for each layer.
             lam: Regularization parameter.
 
         Returns:
@@ -112,21 +110,21 @@ class HomemadeNN(object):
         # initialize matrices
         delta_mats = []
         d = []
-        for i in range(len(w_mats)):
-            delta_mats.append(np.zeros(w_mats[i].shape))
-            d.append(np.zeros((x_data.shape[0],w_mats[i].shape[1])))
+        for i in range(len(self.w_mats)):
+            delta_mats.append(np.zeros(self.w_mats[i].shape))
+            d.append(np.zeros((x_data.shape[0],self.w_mats[i].shape[1])))
         D_mats = delta_mats[:]
 
         # do forward propagation to get all activations
-        a = self.forward_prop(x_data, w_mats)
+        a = self.forward_prop(x_data)
 
         # calculate last d (simple because derivative of cost and activation function match)
         d.append(a[-1] - y_data) #TODO: change here to generalize for different cost and activation functions
 
-        for l in reversed(range(len(w_mats))):
+        for l in reversed(range(len(self.w_mats))):
 
             # select weight matrix
-            w_mat = w_mats[l]
+            w_mat = self.w_mats[l]
 
             # calculate d vector of layer
             wd = d[l+1].dot(w_mat)
@@ -145,13 +143,12 @@ class HomemadeNN(object):
         return D_mats
 
 
-    def cost_fun(self, x_data, y_data, w_mats, lam):
+    def cost_fun(self, x_data, y_data, lam):
         """ Calculate logarithmic cost function.
 
         Args:
             x_data: Input data of shape (data points)x(input layer).
             y_data: Label data of shape (data points)x(output layer).
-            w_mats: Neural networks weights for each layer.
             lam: Regularization parameter.
 
         Returns:
@@ -159,14 +156,14 @@ class HomemadeNN(object):
         """
     
         # calculate cost
-        h = self.forward_prop(x_data, w_mats)[-1]
+        h = self.forward_prop(x_data)[-1]
         cost = -1 / x_data.shape[0] * (np.trace(y_data.dot(np.log(h).T)) + np.trace((1 - y_data).dot(np.log(1 - h).T)))
 
         # add regularization
         reg_cost = 0
         
-        for l in range(len(w_mats)):
-            w_mat = w_mats[l]
+        for l in range(len(self.w_mats)):
+            w_mat = self.w_mats[l]
 
             reg_w_mat = w_mat[:,1:]
             
@@ -180,22 +177,16 @@ class HomemadeNN(object):
         return cost
 
 
-    def grad_descent(self, w_mats, D_mats, alpha):
+    def grad_descent(self, D_mats, alpha):
         """Perform gradient descent.
 
         Args:
-            w_mats: Neural networks weights for each layer.
             D_mats: Derivative of cost w.r.t. weights for each layer.
             alpha: Learning rate.
-
-        Returns:
-            w_mats: Updated NN weights for each layer.
         """
 
-        for l in range(len(w_mats)):
-            w_mats[l] = w_mats[l] - alpha * D_mats[l]
-
-        return w_mats
+        for l in range(len(self.w_mats)):
+            self.w_mats[l] = self.w_mats[l] - alpha * D_mats[l]
 
 
     def shuffle_data(self, x_data, y_data):
@@ -220,13 +211,13 @@ class HomemadeNN(object):
     def test(self, x_data, y_data):
         """Test neural network performance."""
 
-        return np.sum(np.argmax(self.forward_prop(x_data, self.w_mats)[-1], axis=1) == np.argmax(y_data, axis=1))/y_data.shape[0]
+        return np.sum(np.argmax(self.forward_prop(x_data)[-1], axis=1) == np.argmax(y_data, axis=1))/y_data.shape[0]
 
 
     def predict(self, x_data):
         """Predict output based on NN weights."""
 
-        return np.argmax(self.forward_prop(x_data, self.w_mats)[-1], axis=1)
+        return np.argmax(self.forward_prop(x_data)[-1], axis=1)
 
     
     def train(self, x_data, y_data, alpha, lam, max_iter, eps=1e-6, batch_size=0, shuffle_opt=0, quiet=0):
@@ -257,7 +248,7 @@ class HomemadeNN(object):
         y_train = y_data[:]
 
         # monitor cost function convergence
-        self.conv_data = [self.cost_fun(x_data, y_data, self.w_mats, lam)]
+        self.conv_data = [self.cost_fun(x_data, y_data, lam)]
 
         for i in range(max_iter):
 
@@ -276,13 +267,13 @@ class HomemadeNN(object):
                 y_batch = y_train[(b*batch_size):((b+1)*batch_size),:]
 
                 # do back propagation
-                D_mats = self.backward_prop(x_batch, y_batch, self.w_mats, lam)
+                D_mats = self.backward_prop(x_batch, y_batch, lam)
 
                 # do gradient descent
-                self.w_mats = self.grad_descent(self.w_mats, D_mats, alpha)
+                self.grad_descent(D_mats, alpha)
 
             # check convergence
-            self.conv_data.append(self.cost_fun(x_data, y_data, self.w_mats, lam))
+            self.conv_data.append(self.cost_fun(x_data, y_data, lam))
             if abs((self.conv_data[-2]-self.conv_data[-1])/self.conv_data[-2]) < eps:
                 break
 
@@ -291,11 +282,8 @@ class HomemadeNN(object):
                 stop_time = time.time()
                 print("Iteration: %d - Cost: %f - Time: %f - Alpha: %f" % (i, self.conv_data[-1], stop_time - start_time, alpha))
             
-                if (i % 10) == 0:
+                if (i % 10) == 0 or i == max_iter-1:
                     print("Accuracy: %.2f %%" % (self.test(x_data, y_data)*100))
-
-        if not quiet:
-            print('The End.')
             
 
     
